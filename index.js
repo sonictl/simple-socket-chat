4e330613028924f870b7075ad02ce310499306f7
BIBIYES <mousehaozi@outlook.com>
2024-07-26 21:52:40 +0800

    修复浏览器切换后台，在前换回来会出现用户名无法加载的情况，添加调用浏览器消息提醒功能

--- index.js
+++ index.js
@@ -5,63 +5,43 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 2000;
 
-http.listen(port, 
-    function () {
+http.listen(port, function () {
     console.log('listening on *:' + port);
-    }
-);
+});
 
 app.use(express.static(path.join(__dirname, '/')));
 
-/*app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
-});*/
-
-
 var numUsers = 0;
 
-io.on('connection', 
-  function (socket) {
+io.on('connection', function (socket) {
     var addedUser = false;
-  	
-    socket.on('new_username', 
-        function (username) {
-          if (addedUser) return;
-          socket.username = username;
-          
-          ++numUsers;
-          addedUser = true;
-          socket.emit('login', {
+
+    socket.on('new_username', function (username) {
+        console.log('Received new_username:', username);
+        if (addedUser) return;
+        socket.username = username;
+
+        ++numUsers;
+        addedUser = true;
+        socket.emit('login', {
             numUsers: numUsers
-          });
+        });
         io.emit('is online', '&#x1F506 <b>' + socket.username + ' 加入.. </b>' + '<span style="color:DarkGray">当前在线: ' + numUsers + '人</span>');
-        }
-    );
+    });
 
-    socket.on('disconnect', 
-        function (username) {     
-          if (addedUser) {
+    socket.on('disconnect', function () {
+        if (addedUser) {
             --numUsers;
-          io.emit('is online', '&#x1F4A1 <b>' + socket.username + ' 离开.. </b>' + '<span style="color:DarkGray">当前在线: ' + numUsers + '人</span>');
-          }
+            io.emit('is online', '&#x1F4A1 <b>' + socket.username + ' 离开.. </b>' + '<span style="color:DarkGray">当前在线: ' + numUsers + '人</span>');
         }
-    );
+    });
 
-    socket.on('chat message', 
-        function (msg1) {
-        // console.log('message: ' + socket.username + msg1);
-        // io.emit('chat message', msg);
-        var n = msg1.includes("<script>");    // Prohibit javascript code injection
-        if (n == true){
-          io.emit('chat message', '<strong>System</strong>: '+'@' + socket.username + '\'s' + ' message is illegal! check it.');
-        	}
-        else{
-        	io.emit('chat message', '<strong>' + socket.username + '</strong>: ' + msg1);
-          }
+    socket.on('chat message', function (msg1) {
+        var n = msg1.includes("<script>");
+        if (n == true) {
+            io.emit('chat message', '<strong>System</strong>: ' + '@' + socket.username + '\'s' + ' message is illegal! check it.');
+        } else {
+            io.emit('chat message', '<strong>' + socket.username + '</strong>: ' + msg1);
         }
-    );
-  }
-);
-;
-
-
+    });
+});
